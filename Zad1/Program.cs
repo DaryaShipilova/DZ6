dd88a60d9cc21da71476b9c75b04dd79e87ef2d8
Darya <dashulya18@mail.ru>
2022-08-29 19:32:42 +0300

    Тема3. Домашнее задание 6

---
+++ Zad1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь  0, 7, 8, -2, -2 -> 2  1, -7, 567, 89, 223-> 4
+
+Console.Write("Введите несколько чисел: ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+        count++;
+}
+Console.WriteLine("Количество положительных чисел - " + count);
